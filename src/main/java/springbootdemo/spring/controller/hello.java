626509ceb2fa385db9ac0769058b7e16bc475294
suvarchala <suvarchala9tech@gmail.com>
2022-01-21 11:51:23 +0530

    springbootexmp

--- src/main/java/springbootdemo/spring/controller/hello.java
+++ src/main/java/springbootdemo/spring/controller/hello.java
@@ -1,5 +1,4 @@
 package springbootdemo.spring.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -9,6 +8,10 @@ public class hello {
     @GetMapping("/home")
     public String home() {
         return "Hello Meow";
+    }
+    @GetMapping("/pen")
+    public String pen() {
+        return "Welcome to Home";
     }
 }
     
